2c860b2c4cd6bd1fe3551f9f0ab9e83544c9cc89
chd830@naver.com <chd830@naver.com>
2022-07-09 10:59:10 +0900

    Feat : reserve로 접근해서 선택한날짜의 영화를 보여주는 기능 구현

---
+++ lotte-movie-reservation/src/main/java/com/example/lottemoviereservation/controller/ReserveController.java
@@ -0,0 +1,71 @@
+package com.example.lottemoviereservation.controller;
+
+import com.example.lottemoviereservation.dao.ReserveDao;
+import com.example.lottemoviereservation.dto.TheaterDetailDto;
+import com.example.lottemoviereservation.util.CalendarUtil;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@WebServlet("/reserve")
+public class ReserveController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doProcess(req, resp);
+    }
+
+    public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+
+        String param = request.getParameter("param");
+
+        // <th><a href="param=reservedetail&year=?&month=?&day=?">09</a></th>
+        if(param.equals("reserve")) {
+            response.sendRedirect("reserve/reserve.jsp");
+        }
+        else if(param.equals("reservedetail")) {
+            String year = request.getParameter("year");
+            String month = request.getParameter("month");
+            String day = request.getParameter("day");
+
+            String date = year+CalendarUtil.two(month)+CalendarUtil.two(day);
+
+            ReserveDao dao = ReserveDao.getInstance();
+            List<TheaterDetailDto> list = dao.getTheaterDetailList(date);
+            Collections.sort(list, new Comparator<TheaterDetailDto>() {
+                @Override
+                public int compare(TheaterDetailDto o1, TheaterDetailDto o2) {
+                    if(o1.getMovieNo() == o2.getMovieNo()) {
+                        try {
+                            SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+                            Date d1 = format.parse(o1.getTheaterDetailTime());
+                            Date d2 = format.parse(o2.getTheaterDetailTime());
+                            return d1.compareTo(d2);
+                        } catch(Exception e) {
+                            e.printStackTrace();
+                        }
+                        return Integer.compare(o1.getMovieNo(), o2.getMovieNo());
+                    }
+                    return Integer.compare(o1.getMovieNo(), o2.getMovieNo());
+                }
+            });
+
+            JSONObject obj = new JSONObject();
+            obj.put("theaterDetailList", list);
+            response.setContentType("application/x-json; charset=utf-8;");
+            response.getWriter().println(obj);
+        }
+    }
+}
--- lotte-movie-reservation/src/main/webapp/reserve/reserve.jsp
+++ lotte-movie-reservation/src/main/webapp/reserve/reserve.jsp
@@ -11,6 +11,16 @@
     <title>Title</title>
 </head>
 <body>
-
+<h2>CGV강남</h2>
+<hr>
+<table>
+    <tr>
+        <th><a href="<%=request.getContextPath()%>/reserve?param=reservedetail&year=2022&month=9&day=23">09</a></th>
+    </tr>
+</table>
+<br/>
+<div>
+    <div>관람등급 안내</div>
+</div>
 </body>
 </html>
